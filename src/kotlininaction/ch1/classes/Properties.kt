6b8123a15857a573f8f57ed82bec94dc2841a7a4
Nikola Curilovic <ncurilovic@gmail.com>
2017-12-13 19:09:36 +0100

    Add code for ch2 (last are enum and when)

--- src/kotlininaction/ch1/classes/Properties.kt
+++ src/kotlininaction/ch1/classes/Properties.kt
@@ -1,1 +1,17 @@
 package kotlininaction.ch1.classes
+
+class Person(
+        val name: String,
+        var isMarried: Boolean
+)
+
+fun main(args: Array<String>) {
+    val person = Person("Bob", true)
+
+    println(person.name)
+
+    person.isMarried = false
+
+    println(person.isMarried)
+}
+
