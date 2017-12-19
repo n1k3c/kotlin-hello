0037a37e56a358e355e359ccc3cfb8a4f225db95
Nikola Curilovic <ncurilovic@gmail.com>
2017-12-19 17:10:19 +0100

    Regular expressions, local functions

--- src/kotlininaction/ch3/regularexp/Strings.kt
+++ src/kotlininaction/ch3/regularexp/Strings.kt
@@ -1,2 +1,24 @@
 package kotlininaction.ch3.regularexp
 
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
+
+fun main(args: Array<String>) {
+    println("12.345-6.A".split("\\.|-".toRegex()))
+
+    println("12.345-6.A".split(".", "-"))
+
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+
+    val kotlinLogo = """| //
+                       .|//
+                       .|/ \"""
+
+    println(kotlinLogo.trimMargin("."))
+}
